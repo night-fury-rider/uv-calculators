df607296f66d893e419d4e4d09889c290a89ac65
yuvi1422 <5191208+yuvi1422@users.noreply.github.com>
2022-03-12 09:54:44 +0530

    Updated styles of sidebar

--- src/components/Core/UVSidebar/UVSidebar.jsx
+++ src/components/Core/UVSidebar/UVSidebar.jsx
@@ -1,28 +1,48 @@
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+
 import { getErrorInString } from "../../../modules/Errors/ErrorService";
-import React from 'react';
+import { Button } from 'react-bootstrap';
+
+import './UVSidebar.css';
 
 const UVSidebar = props => {
+
+  const navigate = useNavigate();
+
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleNavigation = (event, index, url) => {
+    event.preventDefault();
+    navigate(url, { replace: true });
+    setActiveIndex(index);
+  };
 
   if (!props.links || props.links.length === 0) {
     throw new Error(getErrorInString('Invalid Sidebar Data', null, 'Invalid sidebar data is passed to UVSidebar component'));
   }
 
   return (
-    <nav id="sidebarMenu" className="col-md-3 col-lg-2 d-md-block sidebar collapse">
+    <nav id="sidebarMenu" className="col-md-3 col-lg-2 d-md-block sidebar collapse uv-bg-light-soft">
       <div className="position-sticky pt-3">
         <div className="d-flex">
           <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
         </div>
-        <ul className="nav flex-column h-100">
+
+        <hr />
+
+        <ul className="nav nav-pills flex-column mb-auto">
           {props.links.map((linkObj, linkIndex) => (
-            <li className="nav-item" key={linkIndex}>
-              <Link to={process.env.PUBLIC_URL + '/' + linkObj.href} className='nav-link'>{linkObj.title} </Link>
+            <li className="nav-item" key={linkIndex}
+              onClick={event => { handleNavigation(event, linkIndex, process.env.PUBLIC_URL + '/' + linkObj.href) }}>
+              <Button className={"nav-link link-button " + (linkIndex === activeIndex ? 'active' : '')}>
+                {linkObj.title}
+              </Button>
             </li>
           ))}
         </ul>
       </div>
-    </nav>
+    </nav >
   );
 };
 
